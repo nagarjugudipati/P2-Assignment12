28622a50bd5402125e321db24ef4c19a42c8a850
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-08-29 16:34:01 +0530

    P2-Assignment12

---
+++ Data/First114DbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAssignment12.Data
+{
+    public class First114DbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public First114DbContext() : base("name=First114DbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebAppAssignment12.Models.Comment> Comments { get; set; }
+
+        public System.Data.Entity.DbSet<WebAppAssignment12.Models.Post> Posts { get; set; }
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAssignment12.Models
+{
+    [Table("Comment")]
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
